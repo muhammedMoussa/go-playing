989e6ba090b1f82064dd4530fa6336d3ecb75085
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-03-15 18:11:32 +0200

    playing functions

--- src/github.com/muhammedmoussa/go-playing/main.go
+++ src/github.com/muhammedmoussa/go-playing/main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
-
-	"github.com/muhammedmoussa/go-playing/strutil"
 )
 
 var name = "Muhammed"
 
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum(num1, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
 	// var name = "Muhammed"
-	var age int32 = 25
-	var isCool = true
-	var size float32 = 2.4
+	// var age int32 = 25
+	// var isCool = true
+	// var size float32 = 2.4
 
 	// Shorthand vars
 	// name := "Moussa"
 	// emial := "me@mail.com"
 
-	name, emial := "Moussa", "me@mail.com"
-	fmt.Println(name, emial, isCool, size)
-	fmt.Println(math.Sqrt(2))
-	fmt.Printf("%T\n", age)
-	fmt.Println(strutil.Reverse("olleH"))
+	// name, emial := "Moussa", "me@mail.com"
+	// fmt.Println(name, emial, isCool, size)
+	// fmt.Println(math.Sqrt(2))
+	// fmt.Printf("%T\n", age)
+	fmt.Println(greeting("muhammed"))
+	fmt.Println(getSum(70, 4))
+
 }
