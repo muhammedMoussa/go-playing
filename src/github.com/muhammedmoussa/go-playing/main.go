607cfbf4d7f68995f04ee166957ae796705c8893
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-03-15 17:41:28 +0200

    playing variables

--- src/github.com/muhammedmoussa/go-playing/main.go
+++ src/github.com/muhammedmoussa/go-playing/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+var name = "Muhammed"
+
 func main() {
-	fmt.Println("Hello World")
+	// var name = "Muhammed"
+	var age int32 = 25
+	var isCool = true
+	var size float32 = 2.4
+
+	// Shorthand vars
+	// name := "Moussa"
+	// emial := "me@mail.com"
+
+	name, emial := "Moussa", "me@mail.com"
+	fmt.Println(name, emial, isCool, size)
+	fmt.Printf("%T\n", age)
 }
